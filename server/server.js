7011615df815a76f51b0328b2b24a36fc2933eec
George Koloidi <Battleaxe@bk.ru>
2023-07-29 13:26:16 +0200

    feat: auth on server

--- server/server.js
+++ server/server.js
@@ -1,42 +1,85 @@
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import session from 'express-session';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.get('/api/auth', (req, res) => {
+  const client_id = process.env.CLIENT_ID;
+  const redirect_uri = process.env.REDIRECT_URI;
+  const url = `https://oauth.yandex.com/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`;
+  res.redirect(url);
+});
+
+app.get('/api/auth/callback', async (req, res) => {
+  const code = req.query.code;
+  const client_id = process.env.CLIENT_ID;
+  const client_secret = process.env.CLIENT_SECRET;
+  const redirect_uri = process.env.REDIRECT_URI;
+  try {
+    const response = await axios.post('https://oauth.yandex.com/token', {
+      grant_type: 'authorization_code',
+      code,
+      client_id,
+      client_secret,
+      redirect_uri,
+    });
+    const token = response.data.access_token;
+    req.session.token = token;
+    res.redirect('/');
+  } catch (error) {
+    res.status(500).send({ error: 'Authorization failed' });
+  }
+});
+
+app.get('/api/auth/token', (req, res) => {
+  if (req.session.token) {
+    res.send({ token: req.session.token });
+  } else {
+    res.status(401).send({ error: 'Not authorized' });
+  }
+});
 
 app.get('/upload', async (req, res) => {
+  const { url, token } = req.query;
   try {
-    const response = await axios({
-      method: 'get',
-      url: req.query.url,
-      headers: { Authorization: `OAuth ${req.query.token}` },
-      responseType: 'stream',
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `OAuth ${token}`,
+      },
     });
-
-    response.data.pipe(res);
+    res.send(response.data);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ error: 'Upload request failed' });
   }
 });
 
 app.put('/upload', async (req, res) => {
+  const { url, type, token } = req.query;
+  const file = req.body;
   try {
-    const response = await axios({
-      method: 'put',
-      url: req.query.url,
-      data: req,
+    await axios.put(url, file, {
       headers: {
-        'Content-Type': req.query.type,
-        Authorization: `OAuth ${req.query.token}`,
+        'Content-Type': type,
+        Authorization: `OAuth ${token}`,
       },
-      maxBodyLength: Infinity,
-      maxContentLength: Infinity,
     });
-
-    res.send(response.data);
+    res.send({ message: 'Upload successful' });
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send({ error: 'Upload failed' });
   }
 });
 
